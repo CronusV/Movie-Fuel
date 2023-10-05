2e954545c7ab8bced0be7c989d83d753e10dc4e9
Duncan <duncanstewart96@gmail.com>
2023-10-05 10:40:52 -0400

    added filtered discover and image retrieval functionality

--- utils/movieUtil.js
+++ utils/movieUtil.js
@@ -42,8 +42,59 @@ function searchDataBaseByID(id) {
     });
     return (data);
 }
-searchDataBaseByID(34134)
+// filtered search
+function filteredSearchSimple(genresInclude, genresExclude, orderBy, page) {
+    var fetch = require('node-fetch');
+    var url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=".concat(page, "&sort_by=").concat(orderBy, "&with_genres=").concat(genresInclude, "&without_genres=").concat(genresExclude);
+    var options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: "Bearer ".concat(key)
+        }
+    };
+    var data = new Promise(function (resolve, reject) {
+        fetch(url, options)
+            .then(function (res) { return res.json(); })
+            .then(function (json) {
+            resolve(json);
+        })
+            .catch(function (err) { return console.error('error:' + err); });
+    });
+    return (data);
+}
+// get images "https://image.tmdb.org/t/p/original
+function getImagesByID(id) {
+    var fetch = require('node-fetch');
+    var url = "https://api.themoviedb.org/3/movie/".concat(id, "/images?include_image_language=en&language=en");
+    var options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: "Bearer ".concat(key)
+        }
+    };
+    var data = new Promise(function (resolve, reject) {
+        fetch(url, options)
+            .then(function (res) { return res.json(); })
+            .then(function (json) {
+            resolve(json);
+        })
+            .catch(function (err) { return console.error('error:' + err); });
+    });
+    return (data);
+}
+function buildImageURL(path) {
+    return "https://image.tmdb.org/t/p/original" + path;
+}
+// filteredSearchSimple("12","28,18","revenue.desc",1)
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch(err => console.error('error:' + err));
+getImagesByID(330457)
     .then(function (data) {
     console.log(data);
+    console.log(buildImageURL(data.posters[0].file_path));
 })
     .catch(function (err) { return console.error('error:' + err); });
--- utils/movieUtil.ts
+++ utils/movieUtil.ts
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: require('find-config')('.env') });
 
 const key:string|undefined = process.env.TMDB_accessToken
-function searchDataBaseByQuery(query, language = 'en-US', page = 1): Promise<object> {
+function searchDataBaseByQuery(query: string, language: string = 'en-US', page: number = 1): Promise<object> {
     const fetch = require('node-fetch');
     const url: string = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=${language}&page=${page}`
     const options:object = {
@@ -21,7 +21,8 @@ function searchDataBaseByQuery(query, language = 'en-US', page = 1): Promise<obj
       })
     return(data)
 }
-function searchDataBaseByID(id): Promise<object> {
+
+function searchDataBaseByID(id: number): Promise<object> {
   const fetch = require('node-fetch');
   const url: string = `https://api.themoviedb.org/3/movie/${id}`
   const options: object = {
@@ -41,8 +42,62 @@ function searchDataBaseByID(id): Promise<object> {
     })
   return(data)
 }
-searchDataBaseByID(34134)
+
+// filtered search
+function filteredSearchSimple(genresInclude: string, genresExclude: string, orderBy: string, page: number): Promise<object> {
+  const fetch = require('node-fetch');
+  const url: string = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${orderBy}&with_genres=${genresInclude}&without_genres=${genresExclude}`
+  const options: object = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${key}`
+      }
+    };
+    let data: Promise<object> = new Promise((resolve,reject) => {
+      fetch(url, options)
+      .then(res => res.json())
+      .then((json) => {
+          resolve(json);
+      })
+      .catch(err => console.error('error:' + err));
+    })
+  return(data)
+}
+
+// get images "https://image.tmdb.org/t/p/original
+
+function getImagesByID(id: number): Promise<object> {
+  const fetch = require('node-fetch');
+  const url: string = `https://api.themoviedb.org/3/movie/${id}/images?include_image_language=en&language=en`
+  const options: object = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${key}`
+      }
+    };
+    let data: Promise<object> = new Promise((resolve,reject) => {
+      fetch(url, options)
+      .then(res => res.json())
+      .then((json) => {
+          resolve(json);
+      })
+      .catch(err => console.error('error:' + err));
+    })
+  return(data)
+}
+function buildImageURL(path: string):string{
+  return "https://image.tmdb.org/t/p/original"+path;
+}
+// filteredSearchSimple("12","28,18","revenue.desc",1)
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch(err => console.error('error:' + err));
+getImagesByID(330457)
 .then((data) => {
-    console.log(data)
+  console.log(data)
+  console.log(buildImageURL(data.posters[0].file_path))
 })
 .catch(err => console.error('error:' + err));
