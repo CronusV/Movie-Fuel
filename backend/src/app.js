45c5d3b1f8852f8448d04790912f5b274811dd13
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-09 10:52:48 -0700

    Added backend folders to src

---
+++ backend/src/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const PORT = 3000;
+
+const server = express();
+
+server.listen(PORT, () => {});
---
+++ backend/src/dao/reviewDAO.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: 'us-east-1',
+});
+
+// Interface to DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
+// This is table name in dynamoDB
+const TableName = 'MovieFuel-Reviews';
+
+//
