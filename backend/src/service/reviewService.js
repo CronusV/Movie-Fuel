ae9effe8952f7c142c69687a9a5c75bc89b62e30
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-09 20:54:08 -0700

    fixed bug where valid body was being rejected:

--- backend/src/service/reviewService.js
+++ backend/src/service/reviewService.js
@@ -10,17 +10,18 @@ function validateReview(req) {
   const validReview = {};
   const body = req.body;
   const invalidMessages = [];
+
   // Input validation
   if (!body.Author || body.Author === '') {
     invalidMessages.push('No attribute Author, or empty Attribute Value');
   }
-  if (body.Title || body.Title === '') {
+  if (!body.Title || body.Title === '') {
     invalidMessages.push('No Title attribute, or empty Title value');
   }
-  if (body.Movie || body.Movie === '') {
+  if (!body.Movie || body.Movie === '') {
     invalidMessages.push('No Movie attribute, or empty Movie value');
   }
-  if (body.Comment || body.Comment === '') {
+  if (!body.Comment || body.Comment === '') {
     invalidMessages.push('No Comment attribute, or empty Comment');
   }
 
