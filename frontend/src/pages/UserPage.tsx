67391d01cfe7d7c5a11571d2a09e4e50af820547
Damion  Dollar <dollardamion@gmail.com>
2023-10-13 13:56:45 -0400

    update

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,136 +1,120 @@
-import React, { useState, useEffect } from 'react';
-import '../user.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getImagesByID from '../utils/movieUtil.js';
+import { setUser } from '../state/userSlice.js'; // Import your user slice actions
+import { Card, Button, Image, Accordion, ListGroup } from 'react-bootstrap';
+import { RootState } from '../state/store.js';
 function UserPage() {
-    const [state, setState] = useState({
-        AboutMe: 'This is my default about me section',
-        editedText: '', // Track edited text
-        isEditing: false, // Track the editing state
-        UserName: "david007",
-        Email: 'ringo@gmail.com',
-        Favorites: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        ProfilePicture: '',
+    const user = useSelector((state: RootState) => state.user);
 
-    });
+    const dispatch = useDispatch();
+
     const apiUrl = 'http://localhost:9000/user/profile';
+
+    const loadFavoriteItems = async (favs: string[]) => {
+
+        const imageUrls = await Promise.all(favs.map(async (item) => getImagesByID(item)));
+        dispatch(setUser({ favoriteItems: imageUrls }));
+    };
+
     useEffect(() => {
-        // Define the username you want to retrieve
         const usernameToRetrieve = 'danny007';
 
-        // Make a GET request to the API endpoint with the username as a parameter
-        fetch(`http://localhost:9000/user/profile/${usernameToRetrieve}`)
-            .then(response => {
+        fetch(`${apiUrl}/${usernameToRetrieve}`)
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 return response.json();
             })
-            .then(data => {
-                console.log(data);
-                setState(data); // Update the state with the fetched user data
-            })
-            .catch(error => {
-                console.error('Error:', error); // Handle and display the error to the user
-            });
-    }, []);
+            .then((data) => {
+                dispatch(setUser(data));
 
+                loadFavoriteItems(data.Favorites);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, [dispatch]);
     function handleAboutMeChange(event: any) {
-        setState({ ...state, editedText: event.target.textContent });
+        dispatch(setUser({ ...user, editedText: event.target.textContent }));
     }
 
     function handleEditClick() {
-        setState({ ...state, isEditing: true, editedText: state.AboutMe });
+        dispatch(setUser({ ...user, isEditing: true, editedText: user.AboutMe }));
     }
 
     function handleCancelClick() {
-        setState({ ...state, isEditing: false, editedText: state.AboutMe });
+        dispatch(setUser({ ...user, isEditing: false, editedText: user.AboutMe }));
     }
 
     function handleSaveClick() {
-        setState({ ...state, isEditing: false, AboutMe: state.editedText });
+        dispatch(setUser({ ...user, isEditing: false, AboutMe: user.editedText }));
     }
 
     return (
-        <div className='container'>
-
-            <img src='logo-color.png' alt='Logo' id='logo' />
-            <div >
-
-                <div id='profile-picture' className="mx-auto">
-
-                    <img id='profile-circle' src={state.ProfilePicture} alt='profile for user' />
+        <>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <div id="profile-picture">
+                    <Image id="profile-circle" src={user.ProfilePicture} alt="profile for user" roundedCircle />
                 </div>
-                <div id='user-box'>
-                    <span id='user-box-content'>
-                        <img src='User.png' />{state.UserName}
-                    </span>
-                </div>
+                <span id="username-box">{user.UserName}</span>
             </div>
-            <div className="accordion" id="accordionPanelsStayOpenExample">
 
-
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <li id='accordions' className="list-group-item" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                            About me
-                            {state.isEditing ? ( // Conditionally render Edit, Save, and Cancel buttons while editing
+            <div id="container">
+                <Card>
+                    {/* About Me Section */}
+                    <Card>
+                        <Card.Header>
+                            About Me
+                            {user.isEditing ? (
                                 <>
-                                    <button onClick={handleSaveClick}>Save</button>
-                                    <button onClick={handleCancelClick}>Cancel</button>
+                                    <Button onClick={handleSaveClick}>Save</Button>
+                                    <Button onClick={handleCancelClick}>Cancel</Button>
                                 </>
                             ) : (
-                                <button id="edit-button" onClick={handleEditClick}>Edit</button>
+                                <Button onClick={handleEditClick}>Edit</Button>
                             )}
-                        </li>
-                    </h2>
-                    <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show">
-                        <div className="accordion-body">
+                        </Card.Header>
+                        <Card.Body>
                             <p
                                 id="editableText"
-                                contentEditable={state.isEditing} // Enable editing only when isEditing is true
+                                contentEditable={user.isEditing}
                                 onBlur={handleAboutMeChange}
                             >
-                                {state.isEditing ? state.editedText : state.AboutMe}
+                                {user.isEditing ? user.editedText : user.AboutMe}
                             </p>
+                        </Card.Body>
+                    </Card>
 
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <li id='accordions' className="list-group-item" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                            Email
-                        </li>
-                    </h2>
-                    <div id="panelsStayOpen-collapseTwo" >
-                        <div className="accordion-body">
-                            <p>{state.Email}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button id='accordions' className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            Favorites
-                        </button>
-                    </h2>
-                    <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse">
-                        <div className="accordion-body">
-                            <ul id='favorite-container'>
-                                {state.Favorites.map(id => (<li key={id}><img id="poster" src='https://image.tmdb.org/t/p/original/mINJaa34MtknCYl5AjtNJzWj8cD.jpg' alt="movie poster" />{id}</li>))}
+                    {/* Email Section */}
+                    <Card>
+                        <Card.Header>Email</Card.Header>
+                        <Card.Body>{user.Email}</Card.Body>
+                    </Card>
 
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                    {/* Favorites Section */}
+                    <Card>
+                        <Card.Header>Favorites</Card.Header>
+                        <ListGroup>
+                            {user.favoriteItems && user.favoriteItems.length > 0 ? (
+                                user.favoriteItems.map((item: any, index: any) => (
+                                    <ListGroup.Item key={index}>
+                                        <img id="profile-circle" src={item} alt="Favorite" />
+                                        maybe the movie description
+                                    </ListGroup.Item>
+                                ))
+                            ) : (
+                                <ListGroup.Item>No favorite items available</ListGroup.Item>
+                            )}
+                        </ListGroup>
+                    </Card>
+                </Card>
             </div>
-
-
-        </div>
-    )
+        </>
+    );
 }
 
-export default UserPage
+export default UserPage;
+
+
