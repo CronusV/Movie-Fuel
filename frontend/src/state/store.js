42d663cfed144e9d1221f51b771d52de669d140a
Damion  Dollar <dollardamion@gmail.com>
2023-10-15 18:02:50 -0400

    guest view, and navbar links updated

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -4,32 +4,6 @@ exports.store = void 0;
 var toolkit_1 = require("@reduxjs/toolkit");
 var userSlice_1 = require("./userSlice");
 var guestSlice_1 = require("./guestSlice");
-var initialState = {
-    user: {
-        AboutMe: 'This is my default about me section',
-        editedText: '',
-        isEditing: false,
-        isFavoritesOpen: false,
-        UserName: 'david007',
-        Email: 'ringo@gmail.com',
-        favoriteItems: [],
-        Favorites: [],
-        ProfilePicture: 'https://static.wikia.nocookie.net/characterprofile/images/c/c8/BotW_Link.png',
-    },
-};
-var initialStateg = {
-    guest: {
-        AboutMe: 'This is my default about me section',
-        editedText: '',
-        isEditing: false,
-        isFavoritesOpen: false,
-        UserName: 'danny007',
-        Email: 'ringo@gmail.com',
-        favoriteItems: [],
-        Favorites: [],
-        ProfilePicture: 'https://static.wikia.nocookie.net/characterprofile/images/c/c8/BotW_Link.png',
-    },
-};
 exports.store = (0, toolkit_1.configureStore)({
     reducer: {
         user: userSlice_1.default,
--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,32 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './userSlice';
 import guestReducer from './guestSlice';
-const initialState = {
-  user: {
-    AboutMe: 'This is my default about me section',
-    editedText: '',
-    isEditing: false,
-    isFavoritesOpen: false,
-    UserName: 'david007',
-    Email: 'ringo@gmail.com',
-    favoriteItems: [] as string[],
-    Favorites: [],
-    ProfilePicture: 'https://static.wikia.nocookie.net/characterprofile/images/c/c8/BotW_Link.png',
-  },
-};
-const initialStateg = {
-  guest: {
-    AboutMe: 'This is my default about me section',
-    editedText: '',
-    isEditing: false,
-    isFavoritesOpen: false,
-    UserName: 'danny007',
-    Email: 'ringo@gmail.com',
-    favoriteItems: [] as string[],
-    Favorites: [],
-    ProfilePicture: 'https://static.wikia.nocookie.net/characterprofile/images/c/c8/BotW_Link.png',
-  },
-};
+
 
 
 export const store = configureStore({
