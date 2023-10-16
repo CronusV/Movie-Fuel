999d93d25d5c32256fb0f6a045d35a8a7207131f
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-16 07:56:47 -0700

    fixed bug with date string

--- backend/src/controllers/reviewsRouter.js
+++ backend/src/controllers/reviewsRouter.js
@@ -4,6 +4,7 @@ const uuid = require('uuid');
 const logger = require('../utils/logger');
 const reviewService = require('../service/reviewService');
 const reviewDAO = require('../dao/reviewDAO');
+const likeDAO = require('../dao/likeDAO');
 
 // This adds post to reviews resource
 // TODO MiddleWare to validate user
@@ -17,7 +18,7 @@ router.post('/', async (req, res) => {
     return;
   }
   const postID = uuid.v4();
-  const dateTime = new Date().toString();
+  const dateTime = new Date().toISOString();
   try {
     const dataReview = await reviewDAO.addReview(
       postID,
@@ -37,6 +38,52 @@ router.post('/', async (req, res) => {
   } catch (err) {
     logger.info(`Failed to add ticket to dynamoDB: ${err}`);
     res.status(500).send({ message: 'Failed to add review to database' });
+  }
+});
+// gets revies by postID resource
+// Shouldn't need to validate user for this
+router.get('/:PostID', async (req, res) => {
+  const PostID = req.params.PostID;
+  try {
+    const getIDData = await reviewDAO.getReview(PostID);
+    logger.info(
+      `Successful GET from dynamoDB ${JSON.stringify(getIDData.Items)}`
+    );
+    res
+      .status(200)
+      .send({ message: 'Successful GET from dynamoDB', data: getIDData });
+  } catch (err) {
+    logger.info(`Failed to get from dynamoDB: ${err}`);
+    res.status(500).send({ message: `Failed to get from dynamoDB: ${err}` });
+  }
+});
+// TODO MIDDLEWARE TO ADD USER TO MAKE SURE YOU CAN REFERENCE IT TO POST, FOR NOW SEND THRU BODY
+router.post('/:PostID/likes', async (req, res) => {
+  const PostID = req.params.PostID;
+  // Check to see if they have a valid post
+  try {
+    const postIDData = await reviewDAO.getReview(PostID);
+    logger.info(
+      `Successful GET from dynamoDB ${JSON.stringify(postIDData.Items)}`
+    );
+    if (postIDData.Items.length != 1) {
+      logger.error('PostID not found in DynamoDB');
+      res.status(400).send('PostID not found in DynamoDB');
+    } else {
+      // Const PostLikes = postIDData.Items[0].likes;
+      // Means found valid post, so now check to see if we already have a like
+      // TODO FINISH THIS WITH USER
+      const getLikeData = await likeDAO.getLike();
+      // if !getLikeData.Items.length
+      //  Then means we can create a like item in our like table
+      //  also means we can update likes in our post table
+      // else means we already have like
+      //  This means we delete our like from the table
+      //  Also means we need to update likes in our post table
+    }
+  } catch (err) {
+    logger.info(`Failed to get from dynamoDB: ${err}`);
+    res.status(500).send({ message: `Failed to get from dynamoDB: ${err}` });
   }
 });
 
