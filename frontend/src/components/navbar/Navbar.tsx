6b276524e13f79373a975200ec2dbb708fd6f490
Scott Nelson <snelson@instructors.2u.com>
2023-10-16 06:09:11 -0700

    added login and register pages

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import { Nav, Navbar, Button } from "react-bootstrap";
-
+import { LinkContainer } from "react-router-bootstrap";
 import "./navbar.css";
 
 function Navigation() {
   return (
     <Navbar expand="lg" variant="dark" className="bg-body-tertiary">
       <div className="navbar-items">
-        <Navbar.Brand>Movie Fuel</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand as="a">Movie Fuel</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -15,12 +17,16 @@ function Navigation() {
           </Nav>
         </Navbar.Collapse>
         <div className="navbar-actions">
-          <Button as="a" variant="outline-danger">
-            SIGN IN
-          </Button>
-          <Button as="a" variant="warning">
-            SIGN UP
-          </Button>
+          <LinkContainer to="/login">
+            <Button as="a" variant="outline-danger">
+              SIGN IN
+            </Button>
+          </LinkContainer>
+          <LinkContainer to="/register">
+            <Button as="a" variant="warning">
+              SIGN UP
+            </Button>
+          </LinkContainer>
         </div>
       </div>
     </Navbar>
--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./auth/authSlice";
+
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     // Reducers go here
     // Ex.
     // reviews: postsReducer,
