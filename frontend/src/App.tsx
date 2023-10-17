fea08ef7ef6f104a37a05e86d074299e2b642662
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-16 18:25:07 -0700

    test postpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
+import PostTest from "./pages/PostTest";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/postTest" element={<PostTest/>} />
       </Routes>
     </Container>
   );
