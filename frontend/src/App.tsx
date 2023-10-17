4be43e7468fce4082bc0aa674a1214985cba172f
Duncan <duncanstewart96@gmail.com>
2023-10-17 12:55:02 -0400

    fixed app.tsx and generated necessary js files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
 
 import SearchPagePage from "./pages/SearchPage";
 const props = {text:"barbie",language:"en-US",page:1}
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import GuestProfileView from "./components/guestProfile/GuestProfileView";
 
 function App() {
   return (
@@ -19,8 +21,7 @@ function App() {
         <Route path="/Search" element={<SearchPagePage />}/>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<UserPage />} />
-        <Route path="/guest-profile/:Otherusername" element={<GuestPage />} />
-        <Route path="/post" element={<PostPage />} />
+        <Route path="/guest-profile/:Otherusername" element={<GuestProfileView />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
