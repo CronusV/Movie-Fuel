50a2da93c1b86c66e8a040b866c278f79d85007b
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-16 18:26:29 -0700

    added button to link to postTest

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,17 +1,20 @@
 import { Nav, Navbar, Button } from "react-bootstrap";
-
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
     <Navbar expand="lg" variant="dark" className="bg-body-tertiary">
       <div className="navbar-items">
-        <Navbar.Brand>Movie Fuel</Navbar.Brand>
+        <Navbar.Brand as={Link} to={"/"}>
+          Movie Fuel
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Movies</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
             <Nav.Link href="#link">Reviews</Nav.Link>
+            <Nav.Link as={Link} to={"/postTest"}>PostTest</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         <div className="navbar-actions">
