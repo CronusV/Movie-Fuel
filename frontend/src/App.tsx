fb0ad5b7c551a0b96ef4abee8a747c8b963644d2
Scott Nelson <snelson@instructors.2u.com>
2023-10-17 10:05:07 -0700

    frontend fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
-
-import SearchPagePage from "./pages/SearchPage";
-const props = {text:"barbie",language:"en-US",page:1}
-
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import SearchPagePage from "./pages/SearchPage";
+import UserPage from "./pages/UserPage";
+import GuestPage from "./components/guestProfile/GuestProfileView";
+import PostPage from "./components/postPage/Post";
+
+const props = {text:"barbie",language:"en-US",page:1}
 
 function App() {
   return (
