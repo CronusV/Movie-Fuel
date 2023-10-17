54cdbc66c1a22e9d8209582d792e26e9d8e114f4
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-17 09:35:22 -0700

    Delete frontend/src/state/api/apiSlice.js

--- frontend/src/state/api/apiSlice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiSlice = void 0;
-var react_1 = require("@reduxjs/toolkit/query/react");
-exports.apiSlice = (0, react_1.createApi)({
-    baseQuery: (0, react_1.fetchBaseQuery)({ baseUrl: "http://localhost:4000" }),
-    tagTypes: ["User"],
-    endpoints: function (builder) { return ({}); },
-});
