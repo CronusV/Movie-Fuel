3307ea716c8b218221258b0f127d8e3987cba0a7
Scott Nelson <snelson@instructors.2u.com>
2023-10-20 11:38:57 -0700

    added function go retrieve now playing movies

--- backend/src/utils/movieUtil.js
+++ backend/src/utils/movieUtil.js
@@ -1,4 +1,34 @@
 require('dotenv').config({ path: require('find-config')('.env') });
+
+const baseURL = "https://api.themoviedb.org/3/movie";
+const imagePath = "https://image.tmdb.org/t/p/original";
+const tmdbHeaders = {
+  accept: "application/json",
+  Authorization: process.env.TMDB_KEY,
+};
+
+const getNowPlayingMovies = async (req, res) => {
+  const path = "/now_playing?language=en-US&page=1";
+  const options = {
+    method: "GET",
+    headers: tmdbHeaders,
+  };
+
+  try {
+    const response = await fetch(baseURL + path, options);
+    const json = await response.json();
+    const movies = json.results.map((result) => {
+      result.backdrop_path = imagePath + result.backdrop_path;
+      result.poster_path = imagePath + result.poster_path;
+      return result;
+    });
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 var key = process.env.TMDB_accessToken;
 function searchDataBaseByQuery(query, language, page) {
     if (language === void 0) { language = 'en-US'; }
@@ -104,6 +134,7 @@ module.exports = {
 searchDataBaseByQuery,
 searchDataBaseByID,
 filteredSearchSimple,
+getNowPlayingMovies,
 getImagesByID,
 buildImageURL
 }
