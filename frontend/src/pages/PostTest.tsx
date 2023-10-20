b38bfcc915ce619ebd633d87fccaec8d3e89b5a4
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-20 08:37:21 -0700

    Test page for a post

--- frontend/src/pages/PostTest.tsx
+++ frontend/src/pages/PostTest.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Post from '../components/postPage/Post'
-import { PostInterface } from '../components/postPage/postInterface'
 import SideBarMovie from '../components/postPage/SideBarMovie'
 import Comment from '../components/postPage/Comment'
 import { Col, Container, Row } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { useGetReviewAgainMutation, useGetReviewQuery } from '../state/reviews/reviewsApiSlice'
 import { Review } from '../types/Review'
+import { useGetCommentsFromPostMutation } from '../state/replies/repliesApiSlice'
+import { CommentType } from '../types/Comment'
 // const testPost: Review = {
 //     PostID: "1",
 //     Author: "alex",
@@ -18,26 +19,58 @@ import { Review } from '../types/Review'
 
 const imbdMovie = {imbd: "IMBD value"}
 
+type responsePost = {
+  message: string,
+  data: {
+      data: {
+          Items: Review[]
+      }
+  },
+  Count: number,
+}
+
+type responseComments = {
+  message: string,
+  data: {
+    data: CommentType[]
+  }
+}
 
 function PostTest({...props}: Review) {
- 
-  return (
-    // <>
-    // {/* <h1>Test</h1> */}
-    // {/* <Post {...testPost}></Post> */}
-    // {/* <SideBarMovie {...imbdMovie}/> */}
-    // {/* <Comment reply={true} /> */}
+//  const {data, error, isLoading} = useGetReviewQuery(props.PostID);
+ const [refetchReview] = useGetReviewAgainMutation()
+ const [review, setReview] = React.useState<Review>(props);
+ const [fetchComments] = useGetCommentsFromPostMutation();
+ const [comments, setComments] = React.useState<CommentType[]>([]);
 
+ useEffect(() => {
+  handleFetch()
+  handleFetchComments()
+ },[]);
+
+ const handleFetch = async () => {
+  // Might need to change this for react params for resource
+  const data = await refetchReview(props.PostID) as responsePost;
+  // console.log(data.data.data.Items[0]);
+  // console.log(JSON.stringify(data))
+  setReview(data.data.data.Items[0]);
+ }
+ const handleFetchComments = async () => {
+  const data = await fetchComments(props.PostID) as responseComments;
+  
+  console.log(data);
+  console.log("this is the comments",JSON.stringify(data.data.data))
+  let regularComments = Array.from(data.data.data);
+  // Put comments from oldest to newest
+  setComments(regularComments.reverse());
+ }
+//  console.log(data);
+  return (
     <Container>
       <Row>
         <Col md={8}>
-          <Post {...props}></Post>
-          {/* This is where all comments will go */}
-          <Comment reply={false} />
-          <Comment reply={true} />
-          <Comment reply={false} />
-          <Comment reply={false} />
-        
+          <Post {...props} replies={comments.length}></Post>
+          {comments.length > 0 ? comments.map((comment: CommentType) => (<Comment {...comment} comments={comments} setComments={setComments}/>)) : <div>No comments</div>}
         </Col>
         <Col md={4}>
           <SideBarMovie {...imbdMovie}/>
