7ebff78f9fe2470a2cb9c3755ae27bad33e82759
Damion  Dollar <dollardamion@gmail.com>
2023-10-22 10:45:50 -0500

    added info to readme and more test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
+import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  auth: {
+    token: 'Sample About Me',
+
+  },
+  user: {
+    isLoaded: true
+  }
+};
+const mockStore = configureStore([]);
 test('renders learn react link', () => {
-  // render(<MemoryRouter>
-  //  <App />
-  //</MemoryRouter>);
+  const store = mockStore(initialState); // Initialize the mock store
 
+  render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  );
 });
