3167975781ad96099f4fbde982fe1a1cfff4cfa2
Damion  Dollar <dollardamion@gmail.com>
2023-10-22 15:13:16 -0500

    update

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,32 +1,4 @@
 
-import { render, screen } from '@testing-library/react';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import App from './App';
-import { MemoryRouter } from 'react-router-dom';
-
-
-const initialState = {
-  auth: {
-    token: 'Sample About Me',
-
-  },
-  user: {
-    isLoaded: true
-  }
-};
-const mockStore = configureStore([]);
-test('renders all components', () => {
-  const store = mockStore(initialState); // Initialize the mock store
-
-  render(
-    <Provider store={store}>
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    </Provider>
-  );
-});
 // import React from 'react';
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
