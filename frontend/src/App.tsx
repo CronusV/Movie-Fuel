d23a03bed2331a5b9fcde267a78717804820086f
CronusV <33589161+CronusV@users.noreply.github.com>
2023-10-22 11:39:45 -0700

    cleaned app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
-import PostTest from "./pages/PostTest";
+import PostPage from "./pages/PostPage";
 import ButtonDropdownsExample from "./components/Searchbar";
 import SearchPagePage from "./pages/SearchPage";
 import GuestPage from "./components/guestProfile/GuestProfileView";
@@ -13,27 +13,13 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { Review } from "./types/Review";
 import Reviews from "./pages/Reviews";
-// const props = {text:"barbie",language:"en-US",page:1}
 
-const testPost: Review = {
-  PostID: "7e67ace3-1206-4ee2-b7d8-55c81da9ae8e",
-  Author: "alex",
-  Comment: "This is what I think about the movie",
-  Title: "Its alright",
-  DateTime: "2023-10-12T03:25:46.169Z",
-  Likes: 10,
-  Movie: 123,
-}
 
-//import PostPage from "./components/postPage/Post";
-
-const props = { text: "barbie", language: "en-US", page: 1 };
 
 function App() {
   return (
     <Container>
       <Navbar />
-
       <Routes>
         <Route path="/movies/:tmdb_id" element={<MoviePage/>}/>
         <Route path="/discover/:ids" element={<DiscoverPage/>} />
@@ -42,13 +28,11 @@ function App() {
         <Route path="/profile" element={<UserPage />} />
         <Route path="/guest-profile/:Otherusername" element={<GuestPage />} />
 
-        <Route path="/postTest" element={<PostTest {...testPost}/>} />
         <Route path="/Reviews" element={<Reviews />} />
-        {/* // <Route path="/reviewHome" element={<ReviewHome/>} /> */}
+        <Route path="/Reviews/:PostID" element = {<PostPage/>}></Route>
 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        
       </Routes>
     </Container>
   );
